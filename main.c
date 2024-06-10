6faf7ec767485378d56f800a6174e47c61c93520
mdabioch <mardab1994@gmail.com>
2024-06-10 22:54:29 +0200

    get screen resolution from frame buffer

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#include "screen/screen.h"
+
 int
 main(void)
 {
+    screen_size_t screen_size = screen_size_get();
+    if (screen_size.result) {
+        return -1;
+    }
+
     return 0;
 }
